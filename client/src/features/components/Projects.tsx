dc0d9622732bdfdb19e6ad2c109f6942f60670d9
bigriver <volodsher@gmail.com>
2023-05-07 14:51:57 -0400

    diagonal font-size

--- client/src/features/components/Projects.tsx
+++ client/src/features/components/Projects.tsx
@@ -99,7 +99,6 @@ export default function Projects(): JSX.Element {
           display: 'flex',
           justifyContent: 'space-between',
           flexWrap: 'wrap',
-          // margin: '2rem 0',
         }}
         className={styles.projects}
       >
