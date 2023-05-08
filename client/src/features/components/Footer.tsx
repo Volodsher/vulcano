faeb40e618fa3bea851e074df4a50c81fa79bf40
bigriver <volodsher@gmail.com>
2023-05-08 06:15:51 -0400

    Adding to Footer

--- client/src/features/components/Footer.tsx
+++ client/src/features/components/Footer.tsx
@@ -7,15 +7,15 @@ export default function Footer() {
         position: 'absolute',
         bottom: '0',
         textAlign: 'center',
-        width: '12rem',
+        width: '26rem',
         left: '50%',
-        marginLeft: '-6rem',
+        marginLeft: '-13rem',
         marginTop: '1rem',
         marginBottom: '1rem',
       }}
     >
       {/* &copy; Vulcano.top 2023 */}
-      2023 Volodymyr Sheremeta
+      2023 Volodymyr Sheremeta, Vulcano.top
     </div>
   );
 }
