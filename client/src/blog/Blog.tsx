fc534985e52642b0cee2ecd6b5ca3bf0b8c33503
bigriver <volodsher@gmail.com>
2024-07-13 23:04:53 -0400

    blog

--- client/src/blog/Blog.tsx
+++ client/src/blog/Blog.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-export default async function Blog() {
+// export default async function Blog() {
+
+export default function Blog() {
   // This request should be refetched on every request.
   // Similar to `getServerSideProps`.
   // const dynamicData = await fetch(`https://...`, { cache: 'no-store' });
